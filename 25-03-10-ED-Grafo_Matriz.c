5ab760556a46022f862a54e085f6cafbf794523b
Renan-Mochizuki <renanmochizuki@gmail.com>
2025-04-03 03:29:23 -0300

    Atualizando grafo matriz

--- 25-03-10-ED-Grafo_Matriz.c
+++ 25-03-10-ED-Grafo_Matriz.c
@@ -15,7 +15,7 @@ typedef struct {
 } Grafo;
 
 // Função que retorna o ponteiro para um grafo se passando o número de vértices
-Grafo* CriarGrafo(int maxVertices) {
+Grafo *CriarGrafo(int maxVertices) {
   Grafo *grafo = malloc(sizeof(Grafo));
 
   // Se a alocação não foi bem sucedida ou foi passado menos de um vértice
@@ -58,7 +58,7 @@ Grafo* CriarGrafo(int maxVertices) {
 }
 
 // Função que imprime os valores do grafo
-void ImprimirValores(Grafo * grafo) {
+void ImprimirValores(Grafo *grafo) {
   if (!grafo) {
     printf("Grafo não existe\n");
     return;
@@ -68,7 +68,7 @@ void ImprimirValores(Grafo * grafo) {
 
   printf("Grafo com %d vertices e %d arestas:\n", nVertices, grafo->numArestas);
   // Loop para imprimir os indices dos vertices
-  for(int i = 0; i< nVertices;i++){
+  for (int i = 0; i < nVertices; i++) {
     printf("\t%1i", i);
   }
   for (int i = 0; i < nVertices; i++) {
@@ -82,12 +82,12 @@ void ImprimirValores(Grafo * grafo) {
 }
 
 // Função que limpa o grafo, zerando a matriz e o número de arestas
-void LimparGrafo(Grafo * grafo){
-  if(!grafo) return;
+void LimparGrafo(Grafo *grafo) {
+  if (!grafo) return;
 
   // Loops que percorrem cada item da matriz
-  for(int i = 0; i< grafo->numVertices;i++){
-    for(int j =0; j<grafo->numVertices;i++){
+  for (int i = 0; i < grafo->numVertices; i++) {
+    for (int j = 0; j < grafo->numVertices; j++) {
       grafo->matriz[i][j] = FALSE;
     }
   }
@@ -96,11 +96,11 @@ void LimparGrafo(Grafo * grafo){
 }
 
 // Função que destroi o grafo
-void DestruirGrafo(Grafo * grafo){
-  if(!grafo) return;
+void DestruirGrafo(Grafo *grafo) {
+  if (!grafo) return;
 
   // Loop que limpa cada uma das arrays da matriz
-  for(int i = 0; i< grafo->numVertices;i++){
+  for (int i = 0; i < grafo->numVertices; i++) {
     free(grafo->matriz[i]);
   }
 
@@ -109,7 +109,7 @@ void DestruirGrafo(Grafo * grafo){
 }
 
 // Função que valida os parâmetros passados
-Boolean ValidarParametros(Grafo*grafo, int vertice1, int vertice2){
+Boolean ValidarParametros(Grafo *grafo, int vertice1, int vertice2) {
   if (!grafo || vertice1 < 0 || vertice2 < 0 || vertice1 >= grafo->numVertices || vertice2 >= grafo->numVertices) {
     return FALSE;
   }
@@ -117,11 +117,11 @@ Boolean ValidarParametros(Grafo*grafo, int vertice1, int vertice2){
 }
 
 // Função que insere uma aresta
-Boolean InserirAresta(Grafo* grafo, int vertice1, int vertice2){
+Boolean InserirAresta(Grafo *grafo, int vertice1, int vertice2) {
   if (!ValidarParametros(grafo, vertice1, vertice2)) return FALSE;
 
   // Verificação para garantir que o numArestas não aumente caso a aresta já existir
-  if(grafo->matriz[vertice1][vertice2] == FALSE){
+  if (grafo->matriz[vertice1][vertice2] == FALSE) {
     grafo->matriz[vertice1][vertice2] = TRUE;
     grafo->matriz[vertice2][vertice1] = TRUE;
     grafo->numArestas++;
@@ -131,11 +131,11 @@ Boolean InserirAresta(Grafo* grafo, int vertice1, int vertice2){
 }
 
 // Função que remove uma aresta
-Boolean RemoveAresta(Grafo* grafo, int vertice1, int vertice2){
+Boolean RemoveAresta(Grafo *grafo, int vertice1, int vertice2) {
   if (!ValidarParametros(grafo, vertice1, vertice2)) return FALSE;
-  
+
   // Verificação para garantir que o numArestas não diminua caso a aresta já existir
-  if(grafo->matriz[vertice1][vertice2] != FALSE){
+  if (grafo->matriz[vertice1][vertice2] != FALSE) {
     grafo->matriz[vertice1][vertice2] = FALSE;
     grafo->matriz[vertice2][vertice1] = FALSE;
     grafo->numArestas--;
@@ -144,24 +144,22 @@ Boolean RemoveAresta(Grafo* grafo, int vertice1, int vertice2){
 }
 
 // Função que verifica se uma aresta existe
-Boolean VerificarExistenciaAresta(Grafo* grafo, int vertice1, int vertice2){
+Boolean VerificarExistenciaAresta(Grafo *grafo, int vertice1, int vertice2) {
   if (!ValidarParametros(grafo, vertice1, vertice2)) return FALSE;
-  
+
   // Verifica se a aresta existe
-  if(grafo->matriz[vertice1][vertice2] == TRUE){
+  if (grafo->matriz[vertice1][vertice2] == TRUE) {
     return TRUE;
   }
   return FALSE;
 }
 
 // Função que retorna o grau de um vértice
-int RetornarGrau(Grafo* grafo, int vertice){
+int RetornarGrau(Grafo *grafo, int vertice) {
   if (!ValidarParametros(grafo, vertice, 0)) return -1;
-  
+
   int grau = 0;
-  for (int i=0;i<grafo->numVertices;i++){
-    // Verifica se o vertice é diferente do vertice que está sendo verificado
-    if (i == vertice) continue;
+  for (int i = 0; i < grafo->numVertices; i++) {
     // Verifica se o vertice é vizinho do vertice que está sendo verificado
     if (grafo->matriz[vertice][i] == TRUE) grau++;
   }
@@ -169,11 +167,19 @@ int RetornarGrau(Grafo* grafo, int vertice){
 }
 
 // Função que verifica se um vértice possui vizinhos
-Boolean VerificarPossuiVizinho(Grafo* grafo, int vertice){
-  // Apenas reaproveitando a função RetornarGrau
-  int grau = RetornarGrau(grafo, vertice);
-  if (grau > 0) return TRUE;
+Boolean VerificarPossuiVizinho(Grafo *grafo, int vertice) {
+  if (!ValidarParametros(grafo, vertice, 0)) return FALSE;
+
+  for (int i = 0; i < grafo->numVertices; i++) {
+    if (grafo->matriz[vertice][i] == TRUE) return TRUE;
+  }
   return FALSE;
+}
+
+// Função que retorna o número de arestas
+int RetornarNumeroArestas(Grafo *grafo) { 
+  if(!grafo) return 0;
+  return grafo->numArestas; 
 }
 
 int main(void) {
@@ -184,7 +190,7 @@ int main(void) {
   printf("Digite o tamanho do grafo\n");
   scanf("%d", &tamanhoDigitado);
 
-  Grafo* grafo = CriarGrafo(tamanhoDigitado);
+  Grafo *grafo = CriarGrafo(tamanhoDigitado);
 
   if (!grafo) {
     printf("Erro ao criar o grafo\n");
@@ -195,7 +201,7 @@ int main(void) {
     printf("\nQual acao deseja realizar?\n");
     printf("1 - Inserir uma aresta\n");
     printf("2 - Remover uma aresta\n");
-    printf("3 - Verificar quantas arestas o grafo possui");
+    printf("3 - Verificar quantas arestas o grafo possui\n");
     printf("4 - Verificar se uma aresta existe\n");
     printf("5 - Verificar se um vertice possui vizinhos\n");
     printf("6 - Calcular grau de um vertice\n");
@@ -207,115 +213,116 @@ int main(void) {
     printf("\n");
 
     valorDigitado = 0;
-    
+
     int vertice1, vertice2;
 
     switch (escolha) {
-      case 1:
-        printf("Digite um valor negativo para parar\n");
-        while (valorDigitado >= 0) {
-          printf("Digite o primeiro vertice que deseja conectar\n");
-          scanf("%d", &vertice1);
-
-          if (vertice1 < 0) break;
-
-          printf("Digite o segundo vertice que deseja conectar\n");
-          scanf("%d", &vertice2);
-
-          if (vertice2 < 0) break;
-
-          Boolean funcaoSucedida = InserirAresta(grafo, vertice1, vertice2);
-
-          if (funcaoSucedida)
-            ImprimirValores(grafo);
-          else
-            printf("\nA aresta não foi inserida\n");
-        }
-        break;
-
-      case 2:
-        printf("Digite um valor negativo para parar\n");
-        while (valorDigitado >= 0) {
-          printf("Digite o primeiro vertice da aresta que deseja remover\n");
-          scanf("%d", &vertice1);
-
-          if (vertice1 < 0) break;
-
-          printf("Digite o segundo vertice da aresta que deseja remover\n");
-          scanf("%d", &vertice2);
-
-          if (vertice2 < 0) break;
-          Boolean funcaoSucedida = RemoveAresta(grafo, vertice1, vertice2);
-
-          if (funcaoSucedida)
-            ImprimirValores(grafo);
-          else
-            printf("\nO valor nao foi removido\n");
-        }
-        break;
-
-      case 3:
-        printf("O grafo possui %d arestas\n", grafo->numArestas);
-        break;
-
-      case 4:
-        printf("Digite o primeiro vertice\n");
+    case 1:
+      printf("Digite um valor negativo para parar\n");
+      while (valorDigitado >= 0) {
+        printf("Digite o primeiro vertice que deseja conectar\n");
         scanf("%d", &vertice1);
 
         if (vertice1 < 0) break;
 
-        printf("Digite o segundo vertice\n");
+        printf("Digite o segundo vertice que deseja conectar\n");
         scanf("%d", &vertice2);
 
         if (vertice2 < 0) break;
 
-        Boolean arestaExiste = VerificarExistenciaAresta(grafo, vertice1, vertice2);
+        Boolean funcaoSucedida = InserirAresta(grafo, vertice1, vertice2);
 
-        if (arestaExiste) {
-          printf("A aresta de vertices %d e %d existe\n", vertice1, vertice2);
-        } else {
-          printf("A aresta não existe\n");
-        }
-        break;
+        if (funcaoSucedida)
+          ImprimirValores(grafo);
+        else
+          printf("\nA aresta não foi inserida\n");
+      }
+      break;
 
-      case 5:
-        printf("Digite o vertice\n");
+    case 2:
+      printf("Digite um valor negativo para parar\n");
+      while (valorDigitado >= 0) {
+        printf("Digite o primeiro vertice da aresta que deseja remover\n");
         scanf("%d", &vertice1);
 
         if (vertice1 < 0) break;
 
-        Boolean possuiVizinho = VerificarPossuiVizinho(grafo, vertice1);
+        printf("Digite o segundo vertice da aresta que deseja remover\n");
+        scanf("%d", &vertice2);
 
-        if (possuiVizinho) {
-          printf("O vertice possui vizinhos\n");
-        } else {
-          printf("O vertice nao possui vizinhos\n");
-        }
-        break;
+        if (vertice2 < 0) break;
 
-      case 6:
-        printf("Digite o vertice\n");
-        scanf("%d", &vertice1);
+        Boolean funcaoSucedida = RemoveAresta(grafo, vertice1, vertice2);
 
-        if (vertice1 < 0) break;
+        if (funcaoSucedida)
+          ImprimirValores(grafo);
+        else
+          printf("\nO valor nao foi removido\n");
+      }
+      break;
 
-        int grau = RetornarGrau(grafo, vertice1);
+    case 3:
+      printf("O grafo possui %d arestas\n", RetornarNumeroArestas(grafo));
+      break;
 
-        if (grau >= 0) {
-          printf("O grau do vertice %d e %d\n", vertice1, grau);
-        } else {
-          printf("O vertice nao existe\n");
-        }
-        break;
+    case 4:
+      printf("Digite o primeiro vertice\n");
+      scanf("%d", &vertice1);
 
-      case 7:
-        ImprimirValores(grafo);
-        break;
+      if (vertice1 < 0) break;
 
-      case 8:
-        LimparGrafo(grafo);
-        printf("o Grafo foi limpo\n");
-        break;
+      printf("Digite o segundo vertice\n");
+      scanf("%d", &vertice2);
+
+      if (vertice2 < 0) break;
+
+      Boolean arestaExiste = VerificarExistenciaAresta(grafo, vertice1, vertice2);
+
+      if (arestaExiste) {
+        printf("A aresta de vertices %d e %d existe\n", vertice1, vertice2);
+      } else {
+        printf("A aresta não existe\n");
+      }
+      break;
+
+    case 5:
+      printf("Digite o vertice\n");
+      scanf("%d", &vertice1);
+
+      if (vertice1 < 0) break;
+
+      Boolean possuiVizinho = VerificarPossuiVizinho(grafo, vertice1);
+
+      if (possuiVizinho) {
+        printf("O vertice possui vizinhos\n");
+      } else {
+        printf("O vertice nao possui vizinhos\n");
+      }
+      break;
+
+    case 6:
+      printf("Digite o vertice\n");
+      scanf("%d", &vertice1);
+
+      if (vertice1 < 0) break;
+
+      int grau = RetornarGrau(grafo, vertice1);
+
+      if (grau >= 0) {
+        printf("O grau do vertice %d e %d\n", vertice1, grau);
+      } else {
+        printf("O vertice nao existe\n");
+      }
+      break;
+
+    case 7:
+      ImprimirValores(grafo);
+      break;
+
+    case 8:
+      LimparGrafo(grafo);
+      printf("o Grafo foi limpo\n");
+      break;
     }
   }
 
