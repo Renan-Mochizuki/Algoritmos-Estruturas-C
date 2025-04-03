c0f62ff1eed25f2379daafd903aaa741276ecfce
Renan-Mochizuki <renanmochizuki@gmail.com>
2025-04-03 04:26:50 -0300

    Adicionando bubble sort do outro repositorio

--- 24-06-04-AL-Bubble_Sort.c
+++ 24-06-04-AL-Bubble_Sort.c
@@ -3,13 +3,15 @@
 #include <time.h>
 
 int OrdenarVetor(int vetor[], int tamanho) {
-  int i = 0;
-  int trocou = 0;
+  int i = 0, trocou = 0;
   do {
     trocou = 0;
+    // Subtrai 1 pois fazemos a comparação por pares e 
+    // - i pois a cada iteração i elementos já estão ordenados
     int criterioDoLoop = tamanho - 1 - i;
     for (int j = 0; j < criterioDoLoop; j++) {
       if (vetor[j] > vetor[j + 1]) {
+        // Troca os elementos de lugar
         int auxiliar = vetor[j];
         vetor[j] = vetor[j + 1];
         vetor[j + 1] = auxiliar;
@@ -17,6 +19,8 @@ int OrdenarVetor(int vetor[], int tamanho) {
       }
     }
     i++;
+    // Caso não tenha acontecido nenhuma troca durante toda iteração atual
+    // então, o vetor já está ordenado
   } while (trocou == 1);
 
   return 0;
